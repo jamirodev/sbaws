52bba94b351d1ca508d088e98449d29f4c016818
jamirodev <jamirodev@gmail.com>
2023-08-07 10:51:03 +0900

    oauth

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,9 +1,12 @@
 package com.jojoldu.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jojoldu.book.springboot.service.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,12 +17,22 @@ public class IndexController {
 	private final PostsService postsService;
 	
 	@GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        
         return "index";
     }
 	
 	@GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+	
+	@GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
